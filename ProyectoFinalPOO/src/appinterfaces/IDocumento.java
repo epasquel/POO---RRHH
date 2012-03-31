d320f5eb041b6b8f710b33e3c54837f63c9092b0
Agustin Curu <acuruh@hotmail.com>
2012-03-31 12:30:18 -0500

    Definiendo Documento y IDocumento

---
+++ ProyectoFinalPOO/src/appinterfaces/IDocumento.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appinterfaces;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Administrador
+ */
+public interface IDocumento {
+    
+}
---
+++ ProyectoFinalPOO/src/modelos/Documento.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelos;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
+public class Documento {
+    private Calendar fechaven;
+    private String responsable;
+    private String comentario;
+    private ArrayList usuario;
+}
