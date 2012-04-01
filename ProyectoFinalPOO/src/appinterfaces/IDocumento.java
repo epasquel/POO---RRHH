ac93d17bc8d793f102dcfeb0f5e66c91a98f2577
Agustin Curu <acuruh@hotmail.com>
2012-04-01 13:09:35 -0500

    avance

--- ProyectoFinalPOO/src/appinterfaces/IDocumento.java
+++ ProyectoFinalPOO/src/appinterfaces/IDocumento.java
@@ -12,5 +12,9 @@ import java.util.Calendar;
  * @author Administrador
  */
 public interface IDocumento {
-    
+    public String getCodigo(); // no definido   
+    public abstract String buscar();    
+    public abstract String adicionar();
+    public abstract String editar();
+    public abstract String eliminar();
 }
