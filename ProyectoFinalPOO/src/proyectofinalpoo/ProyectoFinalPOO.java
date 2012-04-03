0b02476c0ebed128e43c4eaa72606f66973cfd20
Carlos Rengifo <crengifof@gmail.com>
2012-04-03 12:22:48 -0500

    Creacion de vistas

--- ProyectoFinalPOO/src/proyectofinalpoo/ProyectoFinalPOO.java
+++ ProyectoFinalPOO/src/proyectofinalpoo/ProyectoFinalPOO.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectofinalpoo;
 
 import controladoras.AdmRol;
@@ -10,9 +7,6 @@ import controladoras.AdmUsuario;
 
 public class ProyectoFinalPOO {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Registrar usuarios //
         AdmUsuario AdmUsuario = new AdmUsuario();
