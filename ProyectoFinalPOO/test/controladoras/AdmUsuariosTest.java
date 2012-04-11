19f579d0219ddef5000dfe1308602b18e1277408
Miguel Angel <sklante_1@hotmail.com>
2012-04-11 00:33:50 -0500

    update

--- ProyectoFinalPOO/test/controladoras/AdmUsuariosTest.java
+++ ProyectoFinalPOO/test/controladoras/AdmUsuariosTest.java
@@ -57,6 +57,33 @@ public class AdmUsuariosTest {
         System.out.print("usario existente");
     }
     
+     @Test
+    public void siElPassWordEsValidoDebeDarmeAcceso(){
+        assertTrue(admUsuarios.validarPassword("sklante","123456"));
+        System.out.println("usted a ingresado correctamente");
+    }
+    
+       
+    @Test
+        
+    public void siElPassWordNoEsValidoNoDebeDarmeAcceso(){
+        assertFalse(admUsuarios.validarPassword("sklante", "1234596"));
+            
+        System.out.println("El password ingresado es incorrecto, por favor ingresar nuevamente el password");
+
+
+    }
+     @Test
+        
+    public void eliminarUsuario(){
+         
+         Permisos permisos = new Permisos(true, true, true, true);
+         Rol newRol= new Rol(permisos);
+     admUsuarios.registrarUsuario("44623912", "Miguel", "Peñaran", "Escalante", "sklante", "mpeñaran@hotmail.com", "01/04/2012", "MiCargo", newRol, "Arquitecto");
+           assertEquals(4 ,admUsuarios.getValores().size()); 
+        System.out.println("El usuario ha sido eliminado");
+
+ 
   
 }
 }
